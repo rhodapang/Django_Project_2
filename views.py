599e018821d08462d06fce48f4c843a8920f5ccb
Rhoda <rhodapang@hotmail.com>
2021-09-01 10:59:37 -0500

    Full-featured web app- database and migration

--- views.py
+++ views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render 
-
-posts = [
-    {
-        'author':'Rhoda',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'data_post': 'August 27, 2018'
-    },
-     {
-        'author':'Asaph',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'data_post': 'August 26, 2018'
-    }
-
-]
+from .models import Post 
 
 
 def home(request):
     context = {
-        'posts': posts 
+        'posts': Post.objects.all() 
     }
     return render(request, 'blog/home.html', context)
 
